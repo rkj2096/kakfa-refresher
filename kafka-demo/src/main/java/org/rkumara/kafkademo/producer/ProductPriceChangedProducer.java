bd431b6d00b3b98b20ca46788b5f8a8b249a3d85
Rohit Kumar <rkj2096@gmail.com>
2025-03-30 06:17:51 +0800

    handle producer error

--- kafka-demo/src/main/java/org/rkumara/kafkademo/producer/ProductPriceChangedProducer.java
+++ kafka-demo/src/main/java/org/rkumara/kafkademo/producer/ProductPriceChangedProducer.java
@@ -1,12 +1,10 @@
 package org.rkumara.kafkademo.producer;
 
-import java.util.concurrent.ExecutionException;
-
+import java.util.concurrent.CompletableFuture;
 import org.rkumara.kafkademo.event.ProductPriceChangedEvent;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,8 +15,14 @@ public class ProductPriceChangedProducer {
     
     private final KafkaTemplate<String, ProductPriceChangedEvent> template;
 
-    public void priceChanged(ProductPriceChangedEvent event) throws InterruptedException, ExecutionException {
-        SendResult<String, ProductPriceChangedEvent> sendResult = template.send("product-price-changes", event).get();
-        log.info("Send result of price changed event: {}", sendResult);
+    public void priceChanged(ProductPriceChangedEvent event) {
+        CompletableFuture<SendResult<String, ProductPriceChangedEvent>> result = template.send("product-price-changes", event);
+        result.whenComplete((sendResult, ex) -> {
+            if (ex != null) {
+                log.error("Error publishing event: {}", ex.getMessage());
+            } else {
+                log.info("Event published successfully: {}", sendResult.getProducerRecord().value());
+            }
+        });
     }
 }
